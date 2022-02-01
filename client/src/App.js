35513edb4a12dbf5496876b69d6239ca5c3c7b42
root <root@ip-172-31-27-199.us-east-2.compute.internal>
2022-02-01 17:22:07 +0000

    wip1

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import FileUpload from './components/FileUpload';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/ResumeNew";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => (
-  <div className='container mt-4' >
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
-    </h4>
+import ScrollToTop from "./components/ScrollToTop";
 
-    <FileUpload />
-  </div>
-);
+function App() {
+  const [load, upadateLoad] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/project" component={Projects} />
+          <Route path="/about" component={About} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
